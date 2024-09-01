b8ecbcbf7fe4dbf954fc456d76fd96dbbf9e9e58
Manpreet Singh <mskhurana729@gmail.com>
2024-09-02 00:49:42 +0530

    feat: Add operate function
    To call other functions.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let firstNumber = 0;
+let operator = '';
+let secondNumber = 0;
+
+
 const add = function (a, b) {
     return a + b;
 };
@@ -6,23 +11,21 @@ const subtract = function (a, b) {
     return a - b;
 };
 
-const sum = function (array) {
-    return array.reduce((total, current) => total + current, 0);
+const divide = function (a, b) {
+    return a / b;
 };
 
 const multiply = function (array) {
     return array.reduce((product, current) => product * current)
 };
 
-const power = function (a, b) {
-    return Math.pow(a, b);
-};
+// We need to create a new function operate that takes the operator which will decide which function to call and the 2 numbers on which the function will apply
 
-const factorial = function (n) {
-    if (n === 0) return 1;
-    let product = 1;
-    for (let i = n; i > 0; i--) {
-        product *= i;
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+': add(num1, num2); break;
+        case '-': subtract(num1, num2); break;
+        case '/': divide(num1, num2); break;
+        case '*': multiply(num1, num2); break;
     }
-    return product;
-};
+}
