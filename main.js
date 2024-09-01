6d486da52423a8fc1e39571f91f854b0d1b14789
Manpreet Singh <mskhurana729@gmail.com>
2024-09-02 01:31:55 +0530

    fix: Fixed operate functions
    Added return function so that it returns the result
    
    Manpreet Singh

--- main.js
+++ main.js
@@ -23,9 +23,9 @@ const multiply = function (array) {
 
 function operate(operator, num1, num2) {
     switch (operator) {
-        case '+': add(num1, num2); break;
-        case '-': subtract(num1, num2); break;
-        case '/': divide(num1, num2); break;
-        case '*': multiply(num1, num2); break;
+        case '+': { return add(num1, num2); break };
+        case '-': { return subtract(num1, num2); break };
+        case '/': { return divide(num1, num2); break };
+        case '*': { return multiply(num1, num2); break };
     }
 }
